cdafe7d584f703847d0a7a05f6c9f72a96ce7f15
cantrkl <salih.terekli@hotmail.com>
2022-06-06 01:09:09 +0300

    -

--- src/components/Landing/HowItWorks_new.vue
+++ src/components/Landing/HowItWorks_new.vue
@@ -1,11 +1,47 @@
 <template>
     <div style="background-color: #fff !important" class="w-100 d-flex align-center flex-column justify-center p-relative pb-12 mb-12">
-        <div class="align-center w-100" style="max-width: 1200px;     justify-content: center; z-index: 2">
-            <div class="sticky-cards-container" ref="sticky-cards-container">
-                <div class="sticky-cards-wrap" ref="sticky-cards-wrap">
-                    <div class="sticky-card" v-for="(item,i) in steps" :key="i"
-                        :style="getStyles">
-                        <span>CARD {{i + 1}}</span>
+        <!--
+        <div class="d-flex w-100 justify-center">
+            <div class="my-3" style="width: 180px; height: 1px; background: linear-gradient(to right, transparent, #952522, transparent);"></div>
+        </div>
+        <span class="font-weight-bold mt-5" style="font-size: 42px;">Nasıl kullanılır?</span>
+        -->
+        <div class="align-center w-100" style="max-width: 1200px; justify-content: center; z-index: 2">
+            <div class="row ma-0 mt-7 mb-0 mb-md-12">
+                <div v-for="(step, i) in steps" :key="i" class="col-12 col-md-3 pa-0 ma-0">
+                    <div class="pa-8 pa-md-2 roboto" :style="$store.state.dimensions.width > 960 ? 'height: 100%;' : ''" >
+                        <div class="step-card"  style="height: 100%;">
+                            <div class="d-flex flex-column pa-8 justify-start text-left w-100">
+                                <span style="font-size: 30px; font-weight: 900">{{i+1}}</span>
+                                <span class="mt-4" style="font-size: 25px; font-weight: 600">Start</span>
+                                <p class="poppins fs-15 pb-0 mb-0" style="line-height: 1.72; color: #6e6e6e;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspend varius enim in eros.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="sticky-cards-container" ref="sticky-cards-container" >
+                <div class="sticky-cards-wrap px-4 px-md-2">
+                    <div class="sticky-card _1" :style="'top: ' + (50 + (i * 20) + ($store.state.dimensions.width > 960 ? 50 : 0)) + 'px;'" v-for="(step, i) in steps" :key="i">
+                        <div class="d-flex  w-100">
+                            <div class="pa-12 w-100">
+                                <div class="row w-100">
+                                    <div class="col-12 col-md-5 pa-0">
+                                        <div class="text-left d-flex flex-column" style="height: 100%;">
+                                            <span :style="'font-size: ' + ($store.state.dimensions.width > 960 ? '60px' : '35px') + '; font-weight: 600'">{{step.title}}</span>
+                                            <div class="flex-fill"></div>
+                                            
+                                            <span class="poppins mt-8 mt-md-2 font-weight-bold">Easy customization</span>
+                                            <p class="poppins fs-15 pb-0 mb-0 pr-5" style="line-height: 1.72; color: #6e6e6e;">{{step.description}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-7 pa-0">
+                                        <img :src="require('@/assets/img/steps/' + step.image)"  class="adv-img mt-6 mt-md-0">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -14,6 +50,7 @@
 </template>
 
 <script>
+import gsap from 'gsap'
 export default {
     name: 'HowItWorks',
     components: {
@@ -26,28 +63,28 @@ export default {
                 {
                     icon: 'ri-settings-5-fill',
                     title: 'Talep Oluştur',
-                    image: 'hiw-1.png',
+                    image: 'hiw1.png',
                     longTitle: 'Dakikalar İçinde Talep Oluştur',
                     description: 'İş talebinizle ilgili kategoriyi seçin ve aradığınız ürünün bilgilerini, çalışma şartlarınızı detaylarıyla belirterek bir talep oluşturun.'
                 },
                 {
                     icon: 'ri-notification-3-fill',
                     title: 'Talebin Tedarikçilere Ulaşsın',
-                    image: 'hiw-2.png',
+                    image: 'hiw2.png',
                     longTitle: 'The standard Lorem Ipsum passage',
                     description: 'Teklifin, Wallcent’te kayıtlı, ilgili kategorideki tüm tedarikçilere anında ulaşır.'
                 },
                 {
                     icon: 'ri-alert-fill',
                     title: 'Tedarikçiler Teklif Versin',
-                    image: 'hiw-1.png',
+                    image: 'hiw3.png',
                     longTitle: 'The standard Lorem Ipsum passage',
                     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet lacus quis neque consequat dapibus. '
                 },
                 {
                     icon: 'ri-lock-2-fill',
                     title: 'Teklifleri İncele, En Uygununu Seç',
-                    image: 'hiw-2.png',
+                    image: 'hiw4.png',
                     longTitle: 'The standard Lorem Ipsum passage',
                     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet lacus quis neque consequat dapibus. '
 
@@ -63,12 +100,25 @@ export default {
 	},
     methods: {
 		handleScroll (event) {
-            console.log(window.scrollY)
+
+            var elements = [ ... document.getElementsByClassName('sticky-card')];
+            elements.forEach((card, i) => {
+                if(i == (elements.length - 1)) return;
+                if(card.getBoundingClientRect().y > 180) {
+                    card.style.transform = ("translate3d(0px, 0px, 0px) scale3d(1,1,1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)");
+                    card.style.background = "#f0f2f4"
+                    return;
+                };
+                let position = gsap.utils.mapRange(500, 150, 1, (0.88 + (i * 0.02)), (card.getBoundingClientRect().y))
+                
+                let colorscale = gsap.utils.mapRange(1000, 150, (240 + (i * 15)), (209 + (i * 15)), (card.getBoundingClientRect().y))
+                let scale3d = "scale3d("+ position +","+ position +",1)"
+                let color = 'rgb('+colorscale+', '+(colorscale + 2)+', '+(colorscale + 4)+')'
+                card.style.transform = ("translate3d(0px, 0px, 0px) " + scale3d + " rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)");
+                card.style.background = color
+            });
+
         },
-        getStyles: (i) => {
-            let scale3d = scale3d(1,1,1)
-            return ("will-change: background, transform; background-color: red; transform: translate3d(0px, 0px, 0px) " + scale3d + " rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;")
-        }
 	},
 }
 </script>
@@ -127,20 +177,41 @@ export default {
         position: sticky;
         top: 5vh;
         display: -ms-grid;
-        display: grid;
         width: 100%;
-        margin-bottom: 10vw;
+        margin-bottom: 20px;
+        /*
         padding: 5vw;
         grid-auto-columns: 1fr;
         grid-column-gap: 64px;
         grid-row-gap: 40px;
+        */
         -ms-grid-columns: 3fr 7fr;
         grid-template-columns: 3fr 7fr;
         -ms-grid-rows: auto;
         grid-template-rows: auto;
-        background-color: #f0f2f4;
-        border: 1px solid red;
-        height: 400px;
+        background-color: #f8fafc;
+        transition-delay: .3s;
+        transition: .3s;
+        border-radius: 10px;
     }
 
+    .adv-img {
+        /* box-shadow: 2px -3px 14px -4px rgba(0,0,0,0.35); */
+        object-fit: contain;
+        width: 100%;
+        border-radius:10px;
+        transition: .6s;
+    }
+
+    .step-card {
+        background-color: #f0f2f4;
+            display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        align-items: flex-start;
+        border-radius: 10px;
+        color: #1d1d1f;
+        font-weight: 400;
+    }
 </style>
--- src/components/Landing/SubscribeForm.vue
+++ src/components/Landing/SubscribeForm.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="w-100d-flex align-center justify-center p-relative py-12" :style="'background: rgb(255, 255, 255) !important; padding-bottom: 100px !important;'">
-        <div class="row ma-0 mx-5 mx-md-auto mt-12 align-center" style="max-width: 1200px;  background:#f0f2f4; border-radius: 10px;   justify-content: center; z-index: 2">
+    <div class="w-100d-flex align-center justify-center p-relative py-12 my-0 my-md-12" :style="'background: rgb(255, 255, 255) !important; '">
+        <div class="row ma-0 mx-5 mx-md-auto align-center" style="max-width: 1200px;  background:#f0f2f4; border-radius: 10px;   justify-content: center; z-index: 2">
             <div class="col-12 col-md-6 pa-0">
                 <div class="pa-5 pa-md-12 ma-6 text-center text-md-left">
-                    <span style="font-size: 55px" class="font-weight-bold black--text">Subscribe to <br class="d-none d-md-block"> stay updated</span>
+                    <span :style="'font-size: ' + ($store.state.dimensions.width > 960 ? '55px' : '30px')" class="font-weight-bold black--text">Subscribe to <br class="d-none d-md-block"> stay updated</span>
                 </div>
             </div>
             <div class="col-12 col-md-6 pa-0">
                 <div class=" px-0 pa-md-12 ma-6 ml-md-0 mt-0 mt-md-6 pl-md-0 pt-0 pt-md-12">
-                    <div class="d-flex" style="padding: 5px; background: #f4f3f7; border-radius: 10px;">
-                        <input placeholder="your@mail.com" class="border-none" style="font-size: 15px; padding: 18px; margin-right: 6px; border-radius: 10px; border: none; background: #fff;     flex: 1 auto;"/>
-                        <button class="white--text one-line border-none cursor-pointer hover-opacity font-weight-bold roboto fs-15 manrope " style="padding: 17px 20px ; background: #952522; border-radius: 10px">Tedarikçi Bul</button>
+                    <div class="d-flex flex-wrap" style="padding: 5px; background: #f4f3f7; border-radius: 10px;">
+                        <input placeholder="your@mail.com" class="border-none mr-0 mr-md-1" style="font-size: 15px; padding: 18px; border-radius: 10px; border: none; background: #fff;     flex: 1 auto;  flex-grow: 20;"/>
+                        <button class="white--text one-line border-none cursor-pointer hover-opacity font-weight-bold roboto fs-15 manrope mt-2 mt-md-0" style="padding: 17px 20px ; background: #952522; border-radius: 10px; flex:1 auto">Tedarikçi Bul</button>
                     </div>
-                    <div class="d-flex ml-3  align-center mt-2">
+                    <div class="d-flex flex-wrap ml-3  align-center mt-2">
                         <div class="d-flex align-center">
                             <i class="ri-check-line mr-1"></i>
                             <span class="fs-13">Free updates</span>
